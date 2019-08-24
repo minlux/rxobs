3b1b6ddcdcdcd9f8dd36bcf84ed018402d9e5640
Manuel Heiß <manuel-heiss@gmx.de>
2019-08-24 12:25:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# RxObs A *RxJS like* observable implementation in C++ (for training purposes)
+
+This pice of code was inspired by the *Federico Knüssel's* great articel explaining the
+the functionality of RxJS's observables.
+See: https://medium.com/@fknussel/a-simple-observable-implementation-c9c809c89c69
+
+As in its article a lot of nested lambda functions with closure expressions are used, which make it
+complicate (for me - as a C/C++ programmer) to read and understand the functionality.
+So for that reason, it tried to translate the implementation into C++.
+
+The aim of this "project" is the same as *Federico Knüssel's* articel was intended to:
+It shall help to understand what's going on under the hood when we work with RxJS!
+In particular it is intendet to those readers with a C/C++ background.
+
+
+As Federico wrote:
+- An observer is just an object with three methods: next which takes in a value,
+  error which takes in an error message and complete with has no arguments.
+
+- An observable is just a function. This function takes in an observer as an argument,
+  and returns a subscription object.
+
+- A subscription object represents a disposable resource, such as the execution of an Observable.
+  In RxJS a subscription object has a bunch of methods such as add and remove, but the most
+  important one is *unsubscribe* which takes no argument and just disposes the resource held by the subscription.
+
+
+In context of this "project":
+- An *observer* is an abstract class, that defines the interface. User has to derive from that clas
+  and implement there respective functionality/behaviour. (See the *IntObserver* in the example.)
+
+- The *observable* is a templated class. It is instanciated using one of the *factory functions*
+  `of`, `from` or `throwError`. The function used for instanciation defines the behaviour of the observable.
+  It takes the values, that shall be emitted to the observer - on subscription.
+  The *observable* starts it's "job" upon subscription of an observer.
+
+- The *subscription object* is returned on subcription to the *observable*. It is used to unsubscribe from
+  the observable. In this implementation the *observable* IS the *subscription object* and it does nothing!
+  So this may be not taken for "truth". Feel free to leave a comment about that ...
+
+
+
+## How to build
+Just compile it wich `g++ main.cpp`.
+
+
+## Output
+When executed, the program outputs the following:
+
+```
+--------------- TEST CASE 'of' ---------------
+Creating a Single-Integer-Observable, that emits a single value befor it completes.
+Now I am going to subscribe to the Single-Integer-Observable.
+IntObs: 1
+IntObs: complete!
+OK, just for testing: I am going to subscribe to the Single-Integer-Observable, a second time...
+ But normally the nothing should happen any more, as the observable should already be completed!
+Now I am going to unsubscribe from the Single-Integer-Observable.
+
+
+--------------- TEST CASE 'from' ---------------
+Creating a Integer-Series-Observable, that emits a series of integer values befor it completes.
+Now I am going to subscribe to the Integer-Series-Observable.
+IntObs: 1
+IntObs: -2
+IntObs: 3
+IntObs: -4
+IntObs: 5
+IntObs: -6
+IntObs: 7
+IntObs: complete!
+OK, just for testing: I am going to subscribe to the Integer-Series-Observable, a second time...
+ But normally the nothing should happen any more, as the observable should already be completed!
+Now I am going to unsubscribe from that Integer-Series-Observable.
+
+
+--------------- TEST CASE 'throwError' ---------------
+Creating a Error-Observable, that emits an error text (c-string) befor it completes.
+Now I am going to subscribe to the Error-Observable.
+IntObs: An error occured!
+IntObs: complete!
+OK, just for testing: I am going to subscribe to the Error-Observable, a second time...
+ But normally the nothing should happen any more, as the observable should already be completed!
+Now I am going to unsubscribe from that Error-Observable.
+```
---
+++ main.cpp
@@ -0,0 +1,288 @@
+//-----------------------------------------------------------------------------
+/*!
+   \file
+   \brief A simple Observable implementation.
+
+   Let’s write our own Observable interface implementation to understand what’s going on under the hood when we work with RxJS.
+
+   An observable is just a function. This function takes in an observer as an argument, and returns a subscription object.
+
+   An observer is just an object with three methods: next which takes in a value, error which takes in an error message and
+   complete with has no arguments.
+
+   A subscription object represents a disposable resource, such as the execution of an Observable. This subscription has a
+   bunch of methods such as add and remove, but the most important one is unsubscribe which takes no argument and just disposes
+   the resource held by the subscription.
+
+   Nach: https://medium.com/@fknussel/a-simple-observable-implementation-c9c809c89c69
+*/
+//-----------------------------------------------------------------------------
+
+/* -- Includes ------------------------------------------------------------ */
+#include <stdint.h>
+#include <iostream>
+#include <string>
+
+
+/* -- Defines ------------------------------------------------------------- */
+using std::cout;
+using std::endl;
+using std::string;
+
+
+/* -- Types --------------------------------------------------------------- */
+
+
+class Subscription
+{
+public:
+   virtual void unsubscribe() = 0;
+
+};
+
+
+template <typename V, typename E>
+class Observer
+{
+public:
+   virtual void next(V const & value) = 0;
+   virtual void error(E const & err) = 0;
+   virtual void complete() = 0;
+};
+
+
+
+template <typename V, typename E>
+class Observable : private Subscription //in this exampel, the Observable also implements the Subscription object...
+{
+private:
+   //typedef for a C++ pointer to a member function
+   //(that takes an pointer to an observer and returns an pointer to a subscription object)
+   typedef Subscription * (Observable::*SubscribeHandler)(Observer<V,E> * observer);
+
+   SubscribeHandler subscribeHandler;
+   V const * values;
+   size_t valuesCount;
+   E const * err;
+
+
+private:
+   //constructor is private - as it shall not be called directly.
+   //instead, a factory function like "from", "of" or "throwError" shall be used!
+   Observable()
+   {
+      this->subscribeHandler = nullptr;
+      this->values = nullptr;
+      this->valuesCount = 0;
+      this->err = nullptr;
+   }
+
+
+   //this is the method that is called when someone subscribes to the observable that was constructed...
+   //...using the "of" method
+   Subscription * subscribeHanlder_of(Observer<V,E> * observer)
+   {
+      //call (the one and only) "next"
+      observer->next(*values);
+      //finally complete
+      observer->complete();
+      //prevent further invocation, by setting the handler fuction to NULL (as the observable has completed now!)
+      subscribeHandler = nullptr;
+      //as Observable derives from Subscription it is very easy at this point to return an Subscription object
+      return this;
+   }
+
+
+   //this is the method that is called when someone subscribes to the observable that was constructed...
+   //...using the "from" method
+   Subscription * subscribeHanlder_from(Observer<V,E> * observer)
+   {
+      //call one "next" after the other
+      for (size_t i = 0; i < valuesCount; i++) observer->next(values[i]);
+      //finally complete
+      observer->complete();
+      //prevent further invocation, by setting the handler fuction to NULL (as the observable has completed now!)
+      subscribeHandler = nullptr;
+      //as Observable derives from Subscription it is very easy at this point to return an Subscription object
+      return this;
+   }
+
+
+   //this is the method that is called when someone subscribes to the observable that was constructed...
+   //...using the "throwError" method
+   Subscription * subscribeHanlder_throwError(Observer<V,E> * observer)
+   {
+      //call error
+      observer->error(*err);
+      //finally complete
+      observer->complete();
+      //prevent further invocation, by setting the handler fuction to NULL (as the observable has completed now!)
+      subscribeHandler = nullptr;
+      //as Observable derives from Subscription it is very easy at this point to return an Subscription object
+      return this;
+   }
+
+
+   //this method implements Subscription::unsubscribe
+   void unsubscribe()
+   {
+      //TBD!?
+      //clear all
+      this->values = nullptr;
+      this->valuesCount = 0;
+      this->err = nullptr;
+   }
+
+
+public:
+
+   //factory function to construct a observable for emits a single value
+   static Observable * of(V const & value) //value is reference to a const V
+   {
+      Observable * thiz = new Observable();
+      thiz->subscribeHandler = &Observable::subscribeHanlder_of;
+      thiz->values = &value;
+      // thiz->valuesCount = 1;
+      return thiz;
+   }
+
+   //factory function to construct a observable for emits a series of values
+   static Observable * from(V const * values, size_t count) //values is pointer to (array of) const V(s)
+   {
+      Observable * thiz = new Observable();
+      thiz->subscribeHandler = &Observable::subscribeHanlder_from;
+      thiz->values = values;
+      thiz->valuesCount = count;
+      return thiz;
+   }
+
+   //factory function to construct a observable for emits an error
+   static Observable * throwError(E const & err)
+   {
+      Observable * thiz = new Observable();
+      thiz->subscribeHandler = &Observable::subscribeHanlder_throwError;
+      thiz->err = &err;
+      return thiz;
+   }
+
+
+   //this method is a wrapper to call the respective subscribe handler method, set at construction
+   Subscription * subscribe(Observer<V,E> & observer)
+   {
+      //if the observable hasn't completed yet...
+      if (this->subscribeHandler != nullptr)
+      {
+         //use c++ function-/method-pointer to...
+         return (this->*subscribeHandler)(&observer); //...call either subscribeHanlder_of/.._from/.._throwError
+      }
+      //otherwise: the observable has already completed
+      //subscription has no further effect, than just returning an Subscription object
+      return this; //as Observable derives from Subscription it is very easy at this point to return an Subscription object
+   }
+};
+
+
+
+
+/* -- (Module) Global Variables ------------------------------------------- */
+
+
+/* -- Implementation ------------------------------------------------------ */
+
+
+
+//demo of an "Integer-Observer" (that takes integers and notifies an character-string in case of error)
+class IntObserver : public Observer<int, char const *>
+{
+private:
+   string id;
+
+public:
+   IntObserver(string id)
+   {
+      this->id = id;
+   }
+
+   void next(int const & value)
+   {
+      cout << id << ": " << value << endl;
+   }
+
+   void error(char const * const & err)
+   {
+      cout << id << ": " << err << endl;
+   }
+
+   void complete()
+   {
+      cout << id << ": complete!" << endl;
+   }
+};
+
+
+
+
+//typedef of an "Integer-Observerable" (that takes integers and notifies an character-string in case of error)
+typedef Observable<int, const char *> IntObservable;
+
+
+int main(int argc, char * argv[])
+{
+   IntObserver myIntObserver("IntObs");
+   Subscription * mySubscription;
+
+   cout << "--------------- TEST CASE 'of' ---------------" << endl;
+   cout << "Creating a Single-Integer-Observable, that emits a single value before it completes." << endl;
+   IntObservable * singleIntObservable = IntObservable::of(1);
+
+   cout << "Now I am going to subscribe to the Single-Integer-Observable." << endl;
+   mySubscription = singleIntObservable->subscribe(myIntObserver);
+
+   cout << "OK, just for testing: I am going to subscribe to the Single-Integer-Observable, a second time..." << endl;
+   cout << " But normally nothing should happen any more, as the observable should already be completed!" << endl;
+   mySubscription = singleIntObservable->subscribe(myIntObserver);
+
+   cout << "Now I am going to unsubscribe from the Single-Integer-Observable." << endl;
+   mySubscription->unsubscribe();
+   cout << endl;
+
+
+
+   cout << "--------------- TEST CASE 'from' ---------------" << endl;
+   cout << "Creating a Integer-Series-Observable, that emits a series of integer values before it completes." << endl;
+   int series[] = { 1, -2, 3, -4, 5, -6, 7 };
+   IntObservable * intSeriesObservable = IntObservable::from(series, 7);
+
+   cout << "Now I am going to subscribe to the Integer-Series-Observable." << endl;
+   mySubscription = intSeriesObservable->subscribe(myIntObserver);
+
+   cout << "OK, just for testing: I am going to subscribe to the Integer-Series-Observable, a second time..." << endl;
+   cout << " But normally nothing should happen any more, as the observable should already be completed!" << endl;
+   mySubscription = intSeriesObservable->subscribe(myIntObserver);
+
+   cout << "Now I am going to unsubscribe from that Integer-Series-Observable." << endl;
+   mySubscription->unsubscribe();
+   cout << endl;
+
+
+   cout << "--------------- TEST CASE 'throwError' ---------------" << endl;
+   cout << "Creating a Error-Observable, that emits an error text (c-string) before it completes." << endl;
+   IntObservable * errorObservable = IntObservable::throwError("An error occured!");
+
+   cout << "Now I am going to subscribe to the Error-Observable." << endl;
+   mySubscription = errorObservable->subscribe(myIntObserver);
+
+   cout << "OK, just for testing: I am going to subscribe to the Error-Observable, a second time..." << endl;
+   cout << " But normally nothing should happen any more, as the observable should already be completed!" << endl;
+   mySubscription = errorObservable->subscribe(myIntObserver);
+
+   cout << "Now I am going to unsubscribe from that Error-Observable." << endl;
+   mySubscription->unsubscribe();
+   cout << endl;
+
+
+
+   cout << endl << "---END---" << endl;
+   return 0;
+}
+
